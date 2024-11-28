a72677f673bb467a995c5337bc52e15559cbe01d
sistemas <sistemas.trasnferbox@gmail.com>
2024-11-28 14:13:58 -0600

    actualizacion tablero general

--- perscripts.js
+++ perscripts.js
@@ -9,4 +9,25 @@ document.querySelectorAll('.btn-primary').forEach(button => {
       this.remove();
     });
   });
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    console.log("Calendario cargado");
+  
+    var calendarEl = document.getElementById('calendario');
+  
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'listWeek', // vista inicial
+      timeZone: 'UTC', // zona horaria
+      themeSystem: 'bootstrap5', // tema de Bootstrap 5
+      weekNumbers: true, // habilitar números de semana
+      dayMaxEvents: true, // permitir el enlace "más" cuando hay demasiados eventos
+      events: 'https://fullcalendar.io/api/demo-feeds/events.json', // fuente de eventos
+      locale: 'es', // Establecer el idioma a español
+      
+    });
+  
+    calendar.render();
+  });
+  
   
